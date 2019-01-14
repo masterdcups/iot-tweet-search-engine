8ca8ca02846cab44bfe84eb94679d702217d7a68
RomainAGEZ <romainagez@gmail.com>
2019-01-14 15:04:00 +0100

    Prediction profil class finished Addition of a requirement

--- prediction_profil.py
+++ prediction_profil.py
@@ -5,68 +5,74 @@ from sklearn.neural_network import MLPClassifier
 
 
 class Prediction:
-    dict = {}
+    """Class to predict users' sentiment, gender and country"""
 
-    def __init__(self, path):  # Notre méthode constructeur
-        """Pour l'instant, on ne va définir qu'un seul attribut"""
-        self.path = path
-
-    def parsingCorpus(self, path):
-        with open(path2, "r") as fichier:
-            line = fichier.readline().replace('\n', '').split('\t')
-            for key in line:
-                dict[key] = []
-            dict['Vecteur'] = []
-            for line in fichier:
-                tweet = line.replace('\n', '').split("\t")
-                dict['TweetID'] += [tweet[0]]
-                dict['Sentiment'] += [tweet[1]]
-                dict['TopicID'] += [tweet[2]]
-                dict['Country'] += [tweet[3]]
-                dict['Gender'] += [tweet[4]]
-                # dict['URLs'] += tweet[5:-2]
-                # dict['Texte'] += tweet[-2]
-                # dict['Vecteur'] += tweet[-1]
-                dict['URLs'] += [tweet[5:]]
-                dict['Vecteur'] += [np.zeros(300)]
-        fichier.close()
+    def __init__(self, map):
+        """this class needs a dictionary with TweetID, Sentiment, TopicID, Country, Gender, URLs, Text, Vector"""
+        self.map = map
 
     @property
-    def genderPrediction(self):
-        X = dict['Vecteur']
-        y = dict['Gender']
+    def gender_prediction(self):
+        """Method to predict user's gender using a SVM classifier"""
+        X = self.map['Vector']
+        y = self.map['Gender']
 
-        # classifieur SVM
         model = svm.SVC(kernel='linear')
         model.fit(X, y)
 
         return model.predict(np.zeros(300).reshape(1, -1))
 
-    print(genderPrediction)
-
     @property
-    def sentimentPrediction(self):
-        X = dict['Vecteur']
-        y = dict['Sentiment']
+    def sentiment_prediction(self):
+        """Method to predict user's sentiment using a CNN"""
+        X = self.map['Vector']
+        y = self.map['Sentiment']
 
-        # CNN - reseau de neurones profonds
         clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
         clf.fit(X, y)
 
         return clf.predict(np.zeros(300).reshape(1, -1))
 
     @property
-    def countryPrediction(self):
-        X = dict['Vecteur']
-        y = dict['Country']
+    def country_prediction(self):
+        """Method to predict user's country using a Naive Bayes network"""
+        X = self.map['Vector']
+        y = self.map['Country']
 
-        # NaiveBayes
         gnb = GaussianNB()
         gnb.fit(X, y)
 
         return gnb.predict(np.zeros(300).reshape(1, -1))
 
+    @staticmethod
+    def parsing_corpus(path):
+        # Les lignes en commentaires devront être décommentées lorsque le corpus sera complété avec le texte des tweets et les vecteurs associés
+        # Les deux lignes avant "fichier.close()" devront alors être supprimées
+        map = {}
+        with open(path, "r") as fichier:
+            line = fichier.readline().replace('\n', '').split('\t')
+            for key in line:
+                map[key] = []
+            map['Vector'] = []
+            for line in fichier:
+                tweet = line.replace('\n', '').split("\t")
+                map['TweetID'] += [tweet[0]]
+                map['Sentiment'] += [tweet[1]]
+                map['TopicID'] += [tweet[2]]
+                map['Country'] += [tweet[3]]
+                map['Gender'] += [tweet[4]]
+                # map['URLs'] += tweet[5:-2]
+                # map['Text'] += tweet[-2]
+                # map['Vector'] += tweet[-1]
+                map['URLs'] += [tweet[5:]]
+                map['Vector'] += [np.zeros(300)]
+        fichier.close()
+        return map
 
 
+if __name__ == '__main__':
+    # truc.txt est un fichier test où il y a les 10eres lignes du corpus
+    map = parsing_corpus('truc.txt')
+    pred = Prediction(map)
 
-    print(sentimentPrediction(dict))
+    print(pred.gender_prediction)
