d9ea538aeeda2eaed9e1512882dbbec0951e69af
RomainAGEZ <romainagez@gmail.com>
2019-02-08 16:43:18 +0100

    creation of web app

--- app/app.py
+++ app/app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-	return "Hello World!"
+@app.route('/index')
+def index():
+	user = {'username': 'Miguel'}
+	posts = [
+		{
+			'author': {'username': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'username': 'Susan'},
+			'body': 'The Avengers movie was so cool!'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
 
 
 if __name__ == '__main__':
