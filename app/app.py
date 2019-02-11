20503f303ce150c355aa9014052b1c050ef12bae
RomainAGEZ <romainagez@gmail.com>
2019-02-11 08:37:49 +0100

    continuity creation of web app

--- app/app.py
+++ app/app.py
@@ -1,24 +1,24 @@
 from flask import Flask
 from flask import render_template
-
+from flask import request
+from iot-tweet-search-engine import user, query_lucene
 app = Flask(__name__)
+
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'username': 'Miguel'}
-	posts = [
-		{
-			'author': {'username': 'John'},
-			'body': 'Beautiful day in Portland!'
-		},
-		{
-			'author': {'username': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}
-	]
-	return render_template('index.html', title='Home', user=user, posts=posts)
+	query = request.form
+	u1 = user.User(id=1, localisation='us', emotion='neutral', gender='andy')
+	if query is None:
+		results = None
+	else:
+		q = query_lucene.QueryLucene()
+		results = q.get_results(nb_results=10)
+
+
+	return render_template('index.html', user=user, posts=results)
 
 
 if __name__ == '__main__':
