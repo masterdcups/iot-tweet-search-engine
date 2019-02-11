44de9a7e751d68542be22f3a985171117a5ae28a
Clément Bosc <hello@clementbosc.fr>
2019-02-11 19:07:36 +0100

    insert tweets into database script

--- insert_into_db.py
+++ insert_into_db.py
@@ -1,14 +1,22 @@
 from dateutil import parser
+from sqlalchemy import func
 
 from app import db
 from app.models.tweet import Tweet
 
 if __name__ == '__main__':
-	f = open('corpus/iot-tweets-vector-10.tsv', 'r')
+	f = open('corpus/iot-tweets-vector-v3.tsv', 'r')
 	f.readline()
+	i = 0
+
+	max = db.session.query(func.max(Tweet.id)).scalar()
+
 	for line in f:
 		line = line[:-1]  # remove \n
 		parts = line.split('\t')
+
+		if int(parts[0]) <= max:
+			continue
 
 		t = Tweet(
 			id=int(parts[0]),
@@ -18,14 +26,17 @@ if __name__ == '__main__':
 			gender=parts[4],
 			urls=parts[5],
 			text=parts[6],
-			user_id=int(parts[7]),
+			user_id=(int(parts[7]) if parts[7] != '' else None),
 			user_name=parts[8],
-			date=parser.parse(parts[9]),
+			date=(parser.parse(parts[9]) if parts[9] != '' else None),
 			hashtags=parts[10],
 			indication=parts[11],
 			cleaned_text=parts[12],
 			vector=[float(x) for x in parts[13][1:-1].split(', ')]
 		)
 		db.session.add(t)
+
+		if i % 1000 == 0:
+			db.session.commit()
 	f.close()
 	db.session.commit()
