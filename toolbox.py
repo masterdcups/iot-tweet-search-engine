c284f4089a419e2c6d0255c37b0d2f26425df591
Clément Bosc <hello@clementbosc.fr>
2019-03-04 15:31:36 +0100

    follower matrix construction and cleanup

--- toolbox.py
+++ toolbox.py
@@ -19,5 +19,31 @@ def format_matrice_likes():
 	f2.close()
 
 
+def format_follow_matrix():
+	f1 = open('corpus/matrice_follower-following.tsv', 'r')
+	f2 = open('corpus/followers_matrix.tsv', 'w')
+
+	f1.readline()
+	# f2.write(f1.readline())
+
+	users = {}
+
+	for line in f1:
+		parts = line[1:-2].split('\t')
+
+		user_name = parts[0]
+		followers = [f[1:-1] for f in parts[1][1:-1].split(', ')]
+		friends = [f[1:-1] for f in parts[2][1:-1].split(', ')]
+
+		if user_name not in users:
+			users[user_name] = followers
+
+	for u in users:
+		f2.write(u + '\t' + '\t'.join(users[u]) + '\n')
+
+	f1.close()
+	f2.close()
+
+
 if __name__ == '__main__':
-	format_matrice_likes()
+	format_follow_matrix()
