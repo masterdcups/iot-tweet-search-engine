5f9c677a903a6d77e78079c9e6c9d0b52c9d747d
kedrisse <kedrisse@hotmail.fr>
2019-03-07 15:48:17 +0100

    tweak_parameters ready to be launched

---
+++ model_prediction.py
@@ -0,0 +1,143 @@
+import os
+from parser import Parser
+
+import pandas as pd
+from definitions import ROOT_DIR
+from joblib import dump, load
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+
+
+class ModelPrediction:
+	path_gend_mod = 'gender_model.joblib'
+	path_sent_mod = 'sentiment_model.joblib'
+	path_coun_mod = 'country_model.joblib'
+
+	def __init__(self, iterator, corpus=None, dir_path=os.path.join(ROOT_DIR, 'saved_models')):
+		"""
+
+		:type corpus: pandas.DataFrame
+		"""
+		self.dir_path = dir_path
+		self.tweets = corpus
+		self.iterator = iterator
+
+		self.X = []
+		self.gender = []
+		self.sentiment = []
+		self.country = []
+
+		for i in self.iterator:
+			vector, sentiment, gender, country = i
+			self.X.append(vector)
+			self.gender.append(gender)
+			self.sentiment.append(sentiment)
+			self.country.append(country)
+
+		self.gender_mod = None
+		self.sentiment_mod = None
+		self.country_mod = None
+
+		if not os.path.exists(dir_path):
+			os.mkdir(dir_path)
+
+	def load_corpus(self):
+		if self.tweets is None:
+			self.tweets = Parser.parsing_vector_corpus_pandas(os.path.join(ROOT_DIR, "corpus/iot-tweets-vector-v3.tsv"))
+
+	def gender_model(self, file_path=path_gend_mod):
+		"""Method to create model prediction user's gender using a SVM classifier"""
+		full_path = os.path.join(self.dir_path, file_path)
+
+		if not os.path.exists(full_path):
+
+			y = self.gender
+
+			self.gender_mod = svm.SVC(kernel='linear')
+			self.gender_mod.fit(self.X, y)
+
+			dump(self.gender_mod, full_path)
+		else:
+			if self.gender_mod is None:
+				self.gender_mod = load(full_path)
+
+		return self.gender_mod
+
+	def sentiment_model(self, file_path=path_sent_mod):
+		"""Method to create model prediction user's sentiment using a CNN"""
+		full_path = os.path.join(self.dir_path, file_path)
+
+		if not os.path.exists(full_path):
+
+			y = self.sentiment
+
+			self.sentiment_mod = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+			self.sentiment_mod.fit(self.X, y)
+
+			dump(self.sentiment_mod, full_path)
+		else:
+			if self.sentiment_mod is None:
+				self.sentiment_mod = load(full_path)
+
+		return self.sentiment_mod
+
+	def country_model(self, file_path=path_coun_mod):
+		"""Method to create model prediction user's country using a Naive Bayes network"""
+		full_path = os.path.join(self.dir_path, file_path)
+
+		if not os.path.exists(full_path):
+
+			y = self.country
+
+			self.country_mod = GaussianNB()
+			self.country_mod.fit(self.X, y)
+
+			dump(self.country_mod, full_path)
+		else:
+			if self.country_mod is None:
+				self.country_mod = load(full_path)
+
+		return self.country_mod
+
+	def tweak_hyperparameters(self, type_model):
+		if type_model == "SVM":
+			y = self.gender
+
+			model = svm.SVC()
+			param_grid = [
+				{'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+				{'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+			]
+		else:
+			y = self.sentiment
+
+			model = MLPClassifier(max_iter=100)
+			param_grid = {
+				'hidden_layer_sizes': [(50, 50, 50), (50, 100, 50), (100,)],
+				'activation': ['tanh', 'relu'],
+				'solver': ['sgd', 'adam'],
+				'alpha': [0.0001, 0.05],
+				'learning_rate': ['constant', 'adaptive'],
+			}
+
+		clf = GridSearchCV(model, param_grid, n_jobs=-1, cv=3)
+		clf.fit(self.X, y)
+
+		print('results of cross-validation :', pd.DataFrame(clf.cv_results_))
+		# Best parameter set
+		print('Best parameters found:\n', clf.best_params_)
+
+
+if __name__ == '__main__':
+	corpus = Parser.parsing_vector_corpus_pandas(os.path.join(ROOT_DIR, 'corpus/iot-tweets-vector-v3.tsv'))
+	model = ModelPrediction(corpus=corpus)
+	model.tweak_hyperparameters("SVM")
+	model.tweak_hyperparameters("MLP")
+# print(model.gender_model())
+# print(model.sentiment_model())
+# print(model.country_model())
+# print(model.gender_model().predict(np.zeros(300).reshape(1, -1)))
+# print(model.sentiment_model().predict(np.zeros(300).reshape(1, -1)))
+# print(model.country_model().predict(np.zeros(300).reshape(1, -1)))
--- prediction_profile.py
+++ prediction_profile.py
@@ -1,76 +1,47 @@
-import numpy as np
-from sklearn import svm
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neural_network import MLPClassifier
+from db import DB
+from models.tweet import Tweet
+
+from model_prediction import ModelPrediction
 
 
 class PredictionProfile:
-    """Class to predict users' sentiment, gender and country"""
+	"""Class to predict users' sentiment, gender and country"""
 
-    def __init__(self, map):
-        """this class needs a dictionary with TweetID, Sentiment, TopicID, Country, Gender, URLs, Text, Vector"""
-        self.map = map
+	def __init__(self, limit: int = None):
+		"""this class needs a dictionary with TweetID, Sentiment, TopicID, Country, Gender, URLs, Text, Vector"""
 
-    def gender_prediction(self, vector):
-        """Method to predict user's gender using a SVM classifier"""
-        X = self.map['Vector']
-        y = self.map['Gender']
+		iterator = PredictionProfile._iterator(limit=limit)
+		self.model_prediction = ModelPrediction(iterator)
 
-        model = svm.SVC(kernel='linear')
-        model.fit(X, y)
+	def gender_prediction(self, vector):
+		"""Method to predict user's gender using a SVM classifier"""
 
-        return model.predict(vector.reshape(1, -1))
+		return self.model_prediction.gender_model().predict(vector.reshape(1, -1))[0]
 
-    def sentiment_prediction(self, vector):
-        """Method to predict user's sentiment using a CNN"""
-        X = self.map['Vector']
-        y = self.map['Sentiment']
+	def sentiment_prediction(self, vector):
+		"""Method to predict user's sentiment using a CNN"""
 
-        clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-        clf.fit(X, y)
+		return self.model_prediction.sentiment_model().predict(vector.reshape(1, -1))[0]
 
-        return clf.predict(vector.reshape(1, -1))
+	def country_prediction(self, vector):
+		"""Method to predict user's country using a Naive Bayes network"""
+		return self.model_prediction.country_model().predict(vector.reshape(1, -1))[0]
 
-    def country_prediction(self, vector):
-        """Method to predict user's country using a Naive Bayes network"""
-        X = self.map['Vector']
-        y = self.map['Country']
+	@staticmethod
+	def _iterator(limit: int = None):
+		query = DB.get_instance().query(Tweet.vector, Tweet.sentiment, Tweet.gender, Tweet.country)
+		if limit is not None:
+			query = query.limit(limit)
 
-        gnb = GaussianNB()
-        gnb.fit(X, y)
-
-        return gnb.predict(vector.reshape(1, -1))
-
-    @staticmethod
-    def parsing_corpus(path):
-        # Les lignes en commentaires devront être décommentées lorsque le corpus sera complété avec le texte des tweets et les vecteurs associés
-        # Les deux lignes avant "fichier.close()" devront alors être supprimées
-        map = {}
-        with open(path, "r") as fichier:
-            line = fichier.readline().replace('\n', '').split('\t')
-            for key in line:
-                map[key] = []
-            map['Vector'] = []
-            for line in fichier:
-                tweet = line.replace('\n', '').split("\t")
-                map['TweetID'] += [tweet[0]]
-                map['Sentiment'] += [tweet[1]]
-                map['TopicID'] += [tweet[2]]
-                map['Country'] += [tweet[3]]
-                map['Gender'] += [tweet[4]]
-                # map['URLs'] += tweet[5:-2]
-                # map['Text'] += tweet[-2]
-                # map['Vector'] += tweet[-1]
-                map['URLs'] += [tweet[5:]]
-                map['Vector'] += [np.zeros(300)]
-        fichier.close()
-        return map
+		for t in query.all():
+			yield t
 
 
 if __name__ == '__main__':
-    # truc.txt est un fichier test où il y a les 10eres lignes du corpus
-    map = PredictionProfile.parsing_corpus('truc.txt')
-    print(map)
-    pred = PredictionProfile(map)
+	pred = PredictionProfile(limit=1000)
 
-    print(pred.gender_prediction(np.zeros(300)))
+	pred.model_prediction.tweak_hyperparameters("SVM")
+# pred.model_prediction.tweak_hyperparameters("MLP")
+#  print(pred.gender_prediction(np.zeros(300)))
+#  print(pred.sentiment_prediction(np.zeros(300)))
+#  print(pred.country_prediction(np.zeros(300)))
